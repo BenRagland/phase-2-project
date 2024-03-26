322c5f325e30467b66428b2734d57ef5c16b8cfe
Michael DiPasquale <michaeldipasquale313@gmail.com>
2024-03-26 18:49:44 -0400

    nutrition fetch and filter now work

--- src/Components/MealFilterForm.js
+++ src/Components/MealFilterForm.js
@@ -1,17 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function MealFilterForm() {
+    const APP_ID = "5218fd09";
+    const APP_KEY = "6f026eb8600d0aae8b2a2639aa0e6ec5"; 
+    const [searchTerm, setSearchTerm] = useState('')
     const [calorieMin, setCalorieMin] = useState('');
     const [calorieMax, setCalorieMax] = useState('');
     const [selectedMealType, setSelectedMealType] = useState('');
     const [selectedCuisineType, setSelectedCuisineType] = useState('');
     const [selectedDiet, setSelectedDiet] = useState('');
     const [selectedHealth, setSelectedHealth] = useState('');
+    const [recipes, setRecipes] = useState([]);
 
     const mealTypes = ['Breakfast', 'Lunch', 'Dinner', 'Snack'];
-    const cuisineTypes = ['Asian', 'American', 'Caribbean', 'Central Europe', 'Chinese', 'Eastern Europe', 'French', 'Indian', 'Italian', 'Japanese', 'Kosher', 'Mediterranean', 'Mexican', 'Middle Eastern', 'South American', 'South East Asian'];
+    const cuisineTypes = ['Asian', 'American', 'Caribbean', 'Chinese', 'French', 'Indian', 'Italian', 'Japanese', 'Kosher', 'Mediterranean', 'Mexican'];
+    // const cuisineTypes = ['Asian', 'American', 'Caribbean', 'Central Europe', 'Chinese', 'Eastern Europe', 'French', 'Indian', 'Italian', 'Japanese', 'Kosher', 'Mediterranean', 'Mexican', 'Middle Eastern', 'South American', 'South East Asian'];
     const diets = ['balanced', 'high-fiber', 'high-protein', 'low-carb', 'low-fat', 'low-sodium'];
     const healthOptions = ['dairy-free', 'egg-free', 'gluten-free', 'keto-friendly', 'low-sugar', 'peanut-free', 'vegan', 'vegetarian'];
+
+    useEffect(() => {
+        const fetchRecipes = async () => {
+            try {
+                const paramsObject = {
+                    type: 'any',
+                    beta: true,
+                    q: searchTerm,
+                    app_id: APP_ID,
+                    app_key: APP_KEY,
+                    ...(calorieMin && { calories: `${calorieMin}-${calorieMax}` }),
+                    ...(selectedMealType && { mealType: selectedMealType }),
+                    ...(selectedCuisineType && { cuisineType: selectedCuisineType }),
+                    ...(selectedDiet && { diet: selectedDiet }),
+                    ...(selectedHealth && { health: selectedHealth })
+                };
+        
+                const params = []
+        
+                for (const [label, value] of Object.entries(paramsObject)) {
+                    params.push(`${label}=${value}`)
+                }
+
+                // https://api.edamam.com/api/recipes/v2?type=any&beta=true&q=chicken&app_id=5218fd09&app_key=6f026eb8600d0aae8b2a2639aa0e6ec5&diet=high-protein&health=gluten-free&cuisineType=American&mealType=Breakfast&field=label&field=dietLabels&field=healthLabels&field=cuisineType&field=mealType 
+
+                const fetchUrl = `https://api.edamam.com/api/recipes/v2?${params.join('&')}`;
+        
+                // const response = await fetch(fetchUrl);
+
+                fetch(fetchUrl)
+                .then( res => {
+                        return res.json()
+                })
+                .then( data => {
+                    setRecipes(data.hits);
+                    console.log("Fetched recipes:", data.hits);
+                })
+        
+                // if (!response.ok) {
+                //     throw new Error('Could not fetch data');
+                // }
+        
+                // const data = await response.json();
+                // setRecipes(data.hits);
+                // setSearchTerm(""); //reset search bar after submit
+        
+                // console.log("Fetched recipes:", data.hits);
+        
+            } catch (error) {
+                console.error('Fetch error:', error);
+            }
+        };
+        
+        fetchRecipes();
+    }, [searchTerm, calorieMin, calorieMax, selectedMealType, selectedCuisineType, selectedDiet, selectedHealth]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,34 +1,43 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
 
 
-const SearchBar = () => {
-    const APP_ID = "5218fd09";
-    const APP_KEY = "6f026eb8600d0aae8b2a2639aa0e6ec5"; 
-    const [query, setQuery] = useState("")
-    const [recipes, setRecipes] = useState([])
+// const SearchBar = () => {
+//     const APP_ID = "5218fd09";
+//     const APP_KEY = "6f026eb8600d0aae8b2a2639aa0e6ec5"; 
+//     const [query, setQuery] = useState("")
+//     const [recipes, setRecipes] = useState([])
 
-    useEffect(() => {
-      fetch(`https://api.edamam.com/api/recipes/v2?type=any&beta=true&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&diet=high-protein&diet=low-carb&diet=low-fat&health=gluten-free&health=low-sugar&health=paleo&health=vegan&health=vegetarian&cuisineType=American&cuisineType=Asian&cuisineType=Chinese&cuisineType=Indian&cuisineType=Italian&cuisineType=Japanese&cuisineType=Mediterranean&cuisineType=Mexican&cuisineType=Middle%20Eastern&cuisineType=South%20American&mealType=Breakfast&mealType=Dinner&mealType=Lunch&calories=calories%3D100-1200&random=true&field=label&field=image&field=source&field=ingredients&field=calories`)
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            console.error('Could not fetch data');
-          }
-        })
-        .then(data => {
-          console.log(data);
-          setRecipes(result.data.hits)
-          setQuery("");//reset search bar after submit
-        });
-    }, []);
+//     useEffect(() => {
+//       fetch(`https://api.edamam.com/api/recipes/v2?type=any&beta=true&q=${query}
+//       &app_id=${APP_ID}
+//       &app_key=${APP_KEY}
+//       &diet=high-protein
+//       &health=gluten-free
+//       &cuisineType=American
+//       &mealType=Breakfast
+//       &calories=calories%3D100-1200
+//       &random=true&field=
+//       `)
+//         .then(response => {
+//           if (response.ok) {
+//             return response.json();
+//           } else {
+//             console.error('Could not fetch data');
+//           }
+//         })
+//         .then(data => {
+//           console.log(data);
+//           setRecipes(result.data.hits)
+//           setQuery("");//reset search bar after submit
+//         });
+//     }, []);
 
-    const onChange = (e) => {
-        console.log(e.target.value)
-        setQuery(e.target.value)
-    }
+//     const onChange = (e) => {
+//         console.log(e.target.value)
+//         setQuery(e.target.value)
+//     }
   
-    return null;//JSX
-  };
+//     return null;//JSX
+//   };
 
-  export default SearchBar;
+//   export default SearchBar;

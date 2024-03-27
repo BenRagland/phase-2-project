ef32204304c1cfedc53af34dcfd5ad07e6d349c8
Spencer-Brown80 <157929194+Spencer-Brown80@users.noreply.github.com>
2024-03-27 19:12:46 -0400

    Update ExerciseCard.js

--- src/Components/ExerciseCard.js
+++ src/Components/ExerciseCard.js
@@ -5,10 +5,14 @@ function ExerciseCard({ exercise, isFavorite, onToggleFavorite }) {
         // Call the onToggleFavorite function passed from the parent component
         onToggleFavorite(exercise);
     };
+    //hides video url in link named Video Search
+    const handleVideoClick = () => {
+        window.open(exercise.Video, '_blank');
+    };
 
     return (
-        <div className="card" style={{ position: 'relative', width: '300px' }}>
-            <div className="card-content">
+        <div className="card" style={{ position: 'relative', maxWidth: '100%', maxHeight: '350px'}}>
+            <div className="card-content" style={{ width: '250px', maxHeight: '250px'}}>
                 <button
                     className="favorite-button"
                     style={{
@@ -25,11 +29,18 @@ function ExerciseCard({ exercise, isFavorite, onToggleFavorite }) {
                     &#9733;
                 </button>
                 <h4>{exercise.WorkOut}</h4>
-                <p style={{ lineHeight: '.5' }}>Beg: {exercise['Beginner Sets']}</p>
-                <p style={{ lineHeight: '.5' }}>Int: {exercise['Intermediate Sets']}</p>
-                <p style={{ lineHeight: '.5' }}>Exp: {exercise['Expert Sets']}</p>
-                <p style={{ lineHeight: '1.0' }}>{exercise.Explaination}</p>
-                <p style={{ lineHeight: '1.0' }}>Video: {exercise.Video} </p>
+                <p style={{ lineHeight: '.5'}}>Beg: {exercise['Beginner Sets']}</p>
+                <p style={{ lineHeight: '.5'}}>Int: {exercise['Intermediate Sets']}</p>
+                <p style={{ lineHeight: '.5'}}>Exp: {exercise['Expert Sets']}</p>
+                <p style={{ lineHeight: '1.0'}}>{exercise.Explaination}</p>
+                <p
+                    style={{ lineHeight: '1.0', cursor: 'pointer', textDecoration: 'underline' }}
+                    onClick={handleVideoClick}
+                >
+                    Video Search
+                </p>
+                {/* Hide the actual link */}
+                <span style={{ display: 'none' }}>{exercise.Video}</span>
             </div>
         </div>
     );

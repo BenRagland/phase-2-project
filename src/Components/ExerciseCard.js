2537ab63a675d06819b916318b4d3b74c57497b4
Spencer-Brown80 <157929194+Spencer-Brown80@users.noreply.github.com>
2024-03-27 13:38:13 -0400

    Update ExerciseCard.js

--- src/Components/ExerciseCard.js
+++ src/Components/ExerciseCard.js
@@ -1,22 +1,38 @@
 import React from 'react';
 
-function ExerciseCard({ exercise }) {
-
-
+function ExerciseCard({ exercise, isFavorite, onToggleFavorite }) {
+    const toggleFavorite = () => {
+        // Call the onToggleFavorite function passed from the parent component
+        onToggleFavorite(exercise);
+    };
 
     return (
-        <li className="card">
-        <img src={exercise.video} alt={exercise.workOut} />
-        <h4>{exercise.WorkOut}</h4>
-        <p>{exercise.Intensity_Level}</p> <p>{exercise.SetsInfo}</p>  
-        <p>{exercise.Explaination}</p>
-        </li>
-
-    )
+        <div className="card" style={{ position: 'relative', width: '300px' }}>
+            <div className="card-content">
+                <button
+                    className="favorite-button"
+                    style={{
+                        position: 'absolute',
+                        top: '10px',
+                        right: '10px',
+                        backgroundColor: 'transparent',
+                        border: 'none',
+                        cursor: 'pointer',
+                        color: isFavorite ? 'yellow' : 'black'
+                    }}
+                    onClick={toggleFavorite}
+                >
+                    &#9733;
+                </button>
+                <h4>{exercise.WorkOut}</h4>
+                <p style={{ lineHeight: '.5' }}>Beg: {exercise['Beginner Sets']}</p>
+                <p style={{ lineHeight: '.5' }}>Int: {exercise['Intermediate Sets']}</p>
+                <p style={{ lineHeight: '.5' }}>Exp: {exercise['Expert Sets']}</p>
+                <p style={{ lineHeight: '1.0' }}>{exercise.Explaination}</p>
+                <p style={{ lineHeight: '1.0' }}>Video: {exercise.Video} </p>
+            </div>
+        </div>
+    );
 }
 
-
 export default ExerciseCard;
-
-
-//need to include more data on card from API.   

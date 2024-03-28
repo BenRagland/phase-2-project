ffec416171b05e51f8bd3b40f2f7b55628f7a432
BenRagland <Officialbenragland@gmail.com>
2024-03-28 07:04:43 -0400

    Create Controlled Form and display db datafor today in body field

--- src/Components/DietJournal/DietJournal.js
+++ src/Components/DietJournal/DietJournal.js
@@ -2,14 +2,54 @@ import React, { useState, useEffect } from 'react';
 import styles from "./Dietjournal.module.css"
 
 function DietJournal() {
-    const [entries, setEntries] = useState(() => {
-        const storedEntries = localStorage.getItem('dietEntries');
-        return storedEntries ? JSON.parse(storedEntries) : Array.from({ length: 15 }, () => ({ meal: '', calories: '', category: '', favorite: false, period: 'AM', submitted: false }));
-    });
-    const [allMeals, setAllMeals] = useState([]); // State to store all meals from the API
+    const [entries, setEntries] = useState(() => {});
+
+    // list of meal Entries from DB
+    const [dailyMealEntries, setDailyMealEntries ] = useState([])
+    const [todaysEntries, setTodaysEntries ] = useState([])
+    const [mealFormState, setMealFormState ] = useState(
+        {
+            meal: "",
+            calories: "", 
+            category: "", 
+            favorite: false,
+            period: "AM",
+            submitted: false,
+            dateStamp: "2024-03-27T17"
+        },
+       
+    )
+
+    // State to store all meals from the API
+    const [allMeals, setAllMeals] = useState([]); 
     const categories = ['Breakfast', 'Lunch', 'Dinner', 'Snack'];
 
-    // Fetch all meals from the API
+    useEffect(()=>{
+        fetch('http://localhost:3001/dietJournalEntries')
+           .then( response => response.json())
+           
+           .then( (allEntries) =>{
+                let todaysDate = new Date().toLocaleDateString()
+            
+               console.log(allEntries, "daily Entries")
+               console.log(todaysDate, "Todays date stamp from system")
+                console.log(allEntries[0].dateStamp, "this is entry.dateStamp")
+               const todaysList = allEntries.filter((entry)=>{
+                    return entry.dateStamp === todaysDate ? true : false
+               })
+               console.log(todaysList, "todaysList")
+               setTodaysEntries(todaysList)
+           })
+           .catch (error =>{
+               console.error('Error submitting entry:', error.message);
+
+            })
+       
+    },[])
+    
+    
+
+    // Fetch all meals Raw Data from the API
     useEffect(() => {
         fetchAllMeals();
     }, []);
@@ -18,30 +58,26 @@ function DietJournal() {
         try {
             const response = await fetch('https://api.edamam.com/api/recipes/v2?type=public&app_id=5218fd09&app_key=6f026eb8600d0aae8b2a2639aa0e6ec5');
             if (!response.ok) {
-                throw new Error('Failed to fetch meals');
+                throw new Error('Failed to fetch Daily Meals');
             }
             const data = await response.json();
             // Assuming data contains the recipes
             const meals = data.hits.map(hit => hit.recipe.label);
             setAllMeals(meals); // Set the retrieved meals in state
         } catch (error) {
-            console.error('Error fetching meals:', error.message);
+            console.error('Error fetching Daily meals:', error.message);
         }
     };
 
-    const handleEntryChange = (index, event) => {
-        const { name, value } = event.target;
-        let updatedEntries = [...entries];
-        if (name === "meal") {
-            // Find the meal label in allMeals
-            const selectedMeal = value;
-            updatedEntries[index] = { ...updatedEntries[index], [name]: value };
-            // Assuming there's no direct way to get calories from the API
-            updatedEntries[index].calories = ''; // Clear calories for now
-        } else {
-            updatedEntries[index] = { ...updatedEntries[index], [name]: value };
-        }
-        setEntries(updatedEntries);
+    const handleEntryChange = (event) => {
+        
+    
+        setMealFormState({
+            ...mealFormState,
+            [event.target.name] :event.target.value
+        })
+
+        console.log("Form state", mealFormState)
     };
 
     const handleSubmitEntry = async (index) => {
@@ -74,9 +110,7 @@ function DietJournal() {
         }
     };
 
-    const removeEntry = (index) => {
-        setEntries(prevEntries => prevEntries.filter((_, i) => i !== index));
-    };
+  
 
     // Clear entries at the beginning of each day
     useEffect(() => {
@@ -111,14 +145,15 @@ function DietJournal() {
                         </tr>
                     </thead>
                     <tbody>
-                        {entries.map((entry, index) => (
-                            <tr key={index}>
+
+                            <tr>
+                                {/* Dish Name Input */}
                                 <td style={{ padding: '5px'}}>
                                     <input
                                         type="text"
                                         name="meal"
-                                        value={entry.meal}
-                                        onChange={(event) => handleEntryChange(index, event)}
+                                        value={mealFormState.meal}
+                                        onChange={(event) => handleEntryChange(event)}
                                         list="mealSuggestions"
                                     />
                                     {/* Display suggestions as the user types */}
@@ -128,19 +163,25 @@ function DietJournal() {
                                         ))}
                                     </datalist>
                                 </td>
+
+
+                                {/* Number of Calories Input */}
                                 <td style={{ padding: '5px'}}>
                                     <input
                                         type="number"
                                         name="calories"
-                                        value={entry.calories}
-                                        onChange={(event) => handleEntryChange(index, event)}
+                                        value={mealFormState.calories}
+                                        onChange={(event) => handleEntryChange(event)}
                                     />
                                 </td>
+
+
+                                {/* Dish Category Type Input */}
                                 <td style={{ padding: '5px'}}>
                                     <select
                                         name="category"
-                                        value={entry.category}
-                                        onChange={(event) => handleEntryChange(index, event)}
+                                        value={mealFormState.category}
+                                        onChange={(event) => handleEntryChange(event)}
                                     >
                                         <option value="">Select Category</option>
                                         {categories.map((category, i) => (
@@ -150,15 +191,33 @@ function DietJournal() {
                                         ))}
                                     </select>
                                 </td>
-                                                         
+
+                                {/* Submit & Delete */}
                                 <td style={{ padding: '5px'}}>
-                                {entry.submitted ? "Submitted" : <button onClick={() => handleSubmitEntry(index)} disabled={entry.submitted}>Submit</button>}                                </td>
+                                {/* {entry.submitted ? "Submitted" : <button onClick={() => handleSubmitEntry(index)} disabled={entry.submitted}>Submit</button>}       */}
+                                </td>
                                 <td style={{ padding: '5px'}}>
-                                    <button onClick={() => removeEntry(index)}> X </button>
+                                    <button onClick={(e)=>{""}}> X </button>
                                 </td>
                             </tr>
-                        ))}
+
+                            {
+                                todaysEntries.length > 0 ? 
+                                todaysEntries.map((mealObj)=>{
+                                    return(
+                                        <tr className ={styles.entryRenderRow}>
+                                            <td> {mealObj.meal}</td>
+                                            <td>{mealObj.calories}</td>
+                                            <td>{mealObj.category}</td>
+                                            <button> X</button>
+                                        </tr>
+                                    )
+                                }) :" "
+                            }
+                        
                     </tbody>
+
+
                 </table>
             </div>
               <button> Diet Data </button>                              
